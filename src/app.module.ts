79df7f5a367743a929c90eefdddc8319a17b3865
amanvile <deribaman@gmail.com>
2025-12-08 16:12:58 +0300

    created prisma module to provide db functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, BookmarkModule],
+  imports: [AuthModule, UsersModule, BookmarkModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
